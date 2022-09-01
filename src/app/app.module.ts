70e4a302eaec4861b06797fc417a19b2db2ea8f4
EvgenII Lysenko <genja1988@inbox.ru>
2022-09-01 15:58:01 +0300

    added main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { ImageComponent } from './components/image/image.component';
+import { ImagesComponent } from './components/images/images.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { PopupComponent } from './components/popup/popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent,
+    ImageComponent,
+    ImagesComponent,
+    LoaderComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule
